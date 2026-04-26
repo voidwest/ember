1e2a0bb099e2162b41b825c1674f06e376579959
voidwest <mineofshadow@gmail.com>
2026-04-26 04:24:12 +0300

    Update tensor.rs

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,1 +1,20 @@
 use alloc::vec::Vec;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct CpuTensor {
+    shape: Vec<usize>,
+    strides: Vec<usize>,
+    data: Vec<f32>,
+}
+
+impl CpuTensor {
+    pub fn zeroes(shape: &[usize]) -> Self {
+        let len = shape.iter().product();
+        let strides = Self::compute_strides(shape);
+        Self {
+            shape: shape.into(),
+            strides,
+            data: vec![0.0; len],
+        }
+    }
+}
