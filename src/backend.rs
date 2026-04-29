ce82d01669f667a08546520cb5b589429c944b1a
voidwest <mineofshadow@gmail.com>
2026-04-29 04:04:39 +0300

    Update backend.rs

--- src/backend.rs
+++ src/backend.rs
@@ -1,8 +1,19 @@
-
 use crate::tensor::CpuTensor;
 
-pub trait Backend{
+pub trait Backend {
     type Tensor: Clone + Send + Sync;
     type Error: core::error::Error;
-}
 
+    fn zeroes(&self, shape: &[usize]) -> Result<Self::Tensor, Self::Error>;
+    fn matmul(&self, a: &Self::Tensor, b: &Self::Tensor) -> Result<Self::Tensor, Self::Error>;
+    fn add(&self, a: &Self::Tensor, b: &Self::Tensor) -> Result<Self::Tensor, Self::Error>;
+    fn softmax(&self, x: &Self::Tensor) -> Result<Self::Tensor, Self::Error>;
+    fn gelu(&self, x: &Self::Tensor) -> Result<Self::Tensor, Self::Error>;
+    fn layer_norm(
+        &self,
+        x: &Self::Tensor,
+        weight: &Self::Tensor,
+        bias: &Self::Tensor,
+        eps: f32,
+    ) -> Result<Self::Tensor, Self::Error>;
+}
