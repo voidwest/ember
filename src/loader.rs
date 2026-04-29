fdebb367e6b12cc9546a003cfab627cab50171e2
voidwest <mineofshadow@gmail.com>
2026-04-30 01:00:45 +0300

    Update loader.rs

--- src/loader.rs
+++ src/loader.rs
@@ -3,3 +3,5 @@ use anyhow::{Context, Result};
 use safetensors::SafeTensors;
 use std::collections::HashMap;
 use std::path::Path;
+
+pub fn load_safetensors<P: AsRef<Path>>(path: P) -> Result<HashMap<String, CpuTensor>> {}
