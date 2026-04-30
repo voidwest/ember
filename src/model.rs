769d62b94618bf0741e1c5e954f507190e8dd0bd
voidwest <mineofshadow@gmail.com>
2026-05-01 02:44:56 +0300

    Update model.rs

--- src/model.rs
+++ src/model.rs
@@ -10,4 +10,7 @@ impl<B: Backend> Linear<B> {
     pub fn new(weight: B::Tensor, bias: Option<B::Tensor>) -> Self {
         Self { weight, bias }
     }
+    pub fn forward(&self, backend: &B, x: &B::Tensor) -> Result<B::Tensor, B::Error> {
+        
+    }
 }
