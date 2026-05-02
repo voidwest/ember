ce4a7cadf6c445307065e6c4deba38ad4eb18d39
voidwest <mineofshadow@gmail.com>
2026-05-02 13:55:13 +0300

    Update kv_cache.rs

--- src/kv_cache.rs
+++ src/kv_cache.rs
@@ -1,1 +1,11 @@
 use alloc::vec::Vec;
+
+pub struct KVCache {
+    k: Vec<f32>,
+    v: Vec<f32>,
+    n_layers: usize,
+    n_heads: usize,
+    head_dim: usize,
+    max_seq_len: usize, // how many tokens were written so far
+    cursor: usize,
+}
